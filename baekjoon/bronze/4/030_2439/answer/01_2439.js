c60a26de7d69b4b146c7cf99a603cab9d04b5879
debSeungBin <sb.hong.dev@gmail.com>
2026-01-04 20:07:33 +0900

    feat(solution): add BOJ 2439

---
+++ baekjoon/bronze/4/030_2439/answer/01_2439.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8');
+
+const lineCount = Number(input);
+let output = '';
+
+for (let crrLine = 1; crrLine <= lineCount; crrLine++) {
+    output += `${' '.repeat(lineCount - crrLine)}${'*'.repeat(crrLine)}\n`;
+}
+
+console.log(output);
