ff3973dccb137682976775b6143258cd46984b3b
debSeungBin <sb.hong.dev@gmail.com>
2026-01-04 19:04:23 +0900

    feat(solution): add BOJ 11021

---
+++ baekjoon/bronze/5/027_11021/answer/01_11021.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+const caseCount = Number(input[0]);
+let output = '';
+
+for(let index = 1; index <= caseCount; index++) {
+    const [a, b] = input[index].split(' ').map(Number);
+    output += `Case #${index}: ${a + b}\n`;
+}
+
+console.log(output);
